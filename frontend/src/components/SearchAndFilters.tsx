2502abf19f0d55cba468d078a789f6b358c6f586
thekannman <zrkann@gmail.com>
2025-04-04 01:33:41 -0500

    minor fix

--- frontend/src/components/SearchAndFilters.tsx
+++ frontend/src/components/SearchAndFilters.tsx
@@ -33,7 +33,7 @@ export default function SearchAndFilters({
         <MagnifyingGlassIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
         <input
           type="text"
-          placeholder="Search attractions..."
+          placeholder="Search..."
           value={searchQuery}
           onChange={(e) => onSearchChange(e.target.value)}
           className="w-full pl-10 pr-4 py-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
