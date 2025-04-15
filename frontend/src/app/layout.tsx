32b3f4334b9e72fe6dd88b2906c9e965c1151799
thekannman <zrkann@gmail.com>
2025-04-15 13:48:15 -0500

    add vercel analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Providers from './providers';
 import Navigation from '@/components/Navigation'
 import Footer from '@/components/Footer'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
